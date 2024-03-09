b85f52bb2f02d27e1aaaf2224dfedd31de9e27db
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:20:50 +0500

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,11 @@
+
+let alien_color:string = "red"
+
+if (alien_color == "green") {
+   console.log("5 points")
+} else if (alien_color == "yellow"){
+   console.log("10 points")
+} else { (alien_color == "red")
+   console.log("15 points")
+}
+
